c5beee955a69255d6cc55a73ca87a199d7dd6dc2
Felipe-Gs <felipegs2905@gmail.com>
2023-01-20 13:16:21 -0300

    back end finalizado

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -138,8 +138,11 @@ export async function appRoutes(app: FastifyInstance){
                     SELECT
                         cast(count(*) as float) 
                     FROM habits_week_days HWD
+                    JOIN habits H
+                        ON H.id = HWD.habit_id
                     WHERE
                         HWD.week_day =cast(strftime('%w', D.date/1000.0, 'unixepoch')as int)
+                        AND H.created_at <= D.date
                 )as amount
             FROM days D
         `
